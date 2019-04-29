c7d0a1a2209dec82ea7f9e8a39b47c5e57dd9f6e
Student <example@example.com>
2019-04-29 08:24:26 -0700

    add random fields

--- FriendLetter.Solution/FriendLetter/Models/LetterVariable.cs
+++ FriendLetter.Solution/FriendLetter/Models/LetterVariable.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace FriendLetter.Models
 {
     public class LetterVariable
@@ -24,6 +26,50 @@ namespace FriendLetter.Models
         public void SetSender(string newSender)
         {
             _sender = newSender;
+        }
+
+        public string GetRandomLocation()
+        {
+          string location = "";
+          Random rnd = new Random();
+          int rand = rnd.Next(1,3);
+          if(rand == 1)
+          {
+            location = "Barcelona";
+          }
+          if(rand == 2)
+          {
+            location = "Baghdad";
+          }
+          if(rand == 3)
+          {
+            location = "The Vatican";
+          }
+
+
+          return location;
+        }
+
+        public string GetRandomSouv()
+        {
+          string source = "";
+          Random rnd = new Random();
+          int rand = rnd.Next(1,3);
+          if(rand == 1)
+          {
+            source = "Life size statue";
+          }
+          if(rand == 2)
+          {
+            source = "suitcase of sand";
+          }
+          if(rand == 3)
+          {
+            source = "unknown snake";
+          }
+
+
+          return source;
         }
     }
 }
